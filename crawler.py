bbba76128b22b0676a49e1af078846e87b958aeb
Grant L <grant.jy.li@gmail.com>
2022-10-18 17:16:11 -0400

    crawler.py update
    crawler.py can now find all the links in a page, including the page itself

--- crawler.py
+++ crawler.py
@@ -1,14 +1,32 @@
 import webdev
 
 def crawl(seed):
-    pagesQueue =[seed]
-    seedPage = webdev.read_url(seed)
-    print(seedPage)
+    pages =[seed]
+    length =0
+    websiteName = seed[0:len(seed)-len("N-0.html")]
+    
+    while len(pages) > length:
+        page = webdev.read_url(pages[-1])
+        start = page.find("href=\"")
 
-    while pagesQueue[-1] != "":
+        length = len(pages)
 
+        while start > 0:
+            end = page.find(".html", start)
+            link = websiteName+page[start+8:end+5]
+
+            if not link in pages:
+                pages.append(link)
+
+            start = page.find("href=\"",end)
+    for i in pages:
+        print(i)
+    return len(pages)
+    
+    
 
 crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
+
 # reset any existing data
 # parse all pages that are in the seed website
 # parses through all pages only once
