10189d6385881b28a579d1fdcb349b35918f8c94
Grant L <grant.jy.li@gmail.com>
2022-10-19 13:17:43 -0400

    crawler basically done

--- crawler.py
+++ crawler.py
@@ -1,37 +1,57 @@
 import webdev
-allPages =[]
+
+allPages = []
+
+
+def get_text(content, URL):
+    fName = URL.strip("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/") + ".txt"
+    fileHand = open(fName, "w")
+    print("writing " + fName)
+    start = content.find("<p>")
+    while start > 0:
+        end = content.find("</p>", start)
+        fileHand.write(content[start + 3: end])
+
+        start = content.find("<p>", end)
+
+    fileHand.close()
+
+
 def crawl(seed):
-
     global allPages
-    pages =[seed]
-    length = 0
-    websiteName = seed[0:len(seed)-len("N-X.html")]
+    allPages.append(seed)
+    page = webdev.read_url(allPages[-1])
+    get_text(webdev.read_url(seed), seed)
 
-    while len(pages) > length:
-        page = webdev.read_url(pages[-1])
+    length = 0
+    websiteName = seed[0:len(seed) - len("N-X.html")]
+
+    while len(allPages) > length:
+
         start = page.find("href=\"")
 
-        length = len(pages)
+        length = len(allPages)
 
         while start > 0:
             end = page.find(".html", start)
-            link = websiteName+page[start+8:end+5]
+            link = websiteName + page[start + 8:end + 5]
 
             if not link in allPages:
-                print(f"crawling {link}")
-                allPages.append(link)
+                print("crawling " + link)
+
                 crawl(link)
 
-            start = page.find("href=\"",end)
-    
-crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
+            start = page.find("href=\"", end)
 
-for i in allPages:
-    print(i)
+def time():
+    import time
+    start = time.time()
+    crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
+    end = time.time()
+    print(end - start)
 
 
-# reset any existing data
-# parse all pages that are in the seed website
-# parses through all pages only once
+time()
+
 # returns the number of total pages found in crawl
 
