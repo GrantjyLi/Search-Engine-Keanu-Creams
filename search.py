0bd5d02957bc7f00e50dbd76302ca2d69ad69421
CBovell <venujaperera12@gmail.com>
2022-10-26 00:01:37 -0400

    search complete

--- search.py
+++ search.py
@@ -1,39 +1,120 @@
 import json
 import math
 import os.path
+from pageRank import pageRank
 import searchdata
 
-def get_Query_data(phrase):
-    phrase = phrase.split()  # making phrase into an array
-    queryVector = []
-    basisVector = []
-    queryDict = {}
+#Global Variables
+phrase = []
+queryVector =[]
+basisVector =[]
+queryDict ={}
+csList = []
+idfData = {}
+pageRankData = {}
 
+#Functions
+def insert_List(csInfo):
+    global csList
+    list = csList
+    for i in range(len(list)):
+        if csInfo['score'] > list[i]['score']:
+            prev = list[0:i]
+            post = list[i:]
+            tempList = prev+[csInfo]+post
+            return tempList
+    
+    return list + [csInfo]
+
+
+def calc_CS(queryVector, pageVector, boost):
+    global pageRankData
+    numerator =0
+    qEuclidNorm =0
+    pEuclidNorm =0
+    for i in range(len(queryVector)):
+        numerator += (queryVector[i] * pageVector['score'][i])
+        qEuclidNorm += queryVector[i] **2
+        pEuclidNorm += pageVector['score'][i] **2
+    if qEuclidNorm ==0 or pEuclidNorm ==0:
+        return 0
+    if boost:
+        return (numerator / ((qEuclidNorm**0.5) * (pEuclidNorm**0.5)))*pageRankData[pageVector['url']]
+    return numerator / ((qEuclidNorm**0.5) * (pEuclidNorm**0.5))
+
+
+def fetchPageRank():
+    filePath = os.path.join("pageRank", 'data.json')
+    if os.path.isfile(filePath):
+        data=json.load(open(filePath))
+        return data
+
+
+def init(input, boost):
+    global phrase
+    global idfData
+    global pageRankData
+    global queryVector
+    global basisVector
+    global queryDict
+    global csList
+
+    phrase = []
+    queryVector =[]
+    basisVector =[]
+    queryDict ={}
+    csList = []
+    idfData = {}
+    pageRankData = {}
+
+    if boost:#Loading pagerank if nessasary
+        pageRankData = fetchPageRank()
+        
+    phrase = input.split()
     fHand = open(os.path.join("pageFreqFiles", "IDFData.json"))
-    idfData = json.load(fHand)  # getting IDF values of crawled website
+    idfData = json.load(fHand) # getting IDF values of crawled website
     fHand.close()
 
-    for i in phrase:  # adding every word in phrase to the basis vector and query dictionary
-        if (i + "IDF") in idfData and idfData[i + "IDF"] != 0:
+
+def populateBasisVector():
+    global phrase
+    global basisVector
+    global queryDict
+    global idfData
+    for i in phrase: # adding every word in phrase to the basis vector and query dictionary
+        if (i + "IDF") in idfData and idfData[i+"IDF"] != 0:
             if i not in queryDict:
-                queryDict[i] = 0
+                queryDict[i] =0
                 basisVector.append(i)
-            queryDict[i] += 1
+            queryDict[i] +=1
 
-    for i in queryDict:  # filling the queryVector with Term-frequency values of each word
-        queryVector.append(queryDict[i] / len(phrase))
 
-    for i in range(len(basisVector)):  # making the term-frequency values of each word into TFIDF values
+def populateQueryVector():
+    global idfData
+    global phrase
+    global queryVector
+    global queryDict
+    global basisVector
+    for i in queryDict: # filling the queryVector with Term-frequency values of each word
+        queryVector.append(queryDict[i]/len(phrase))
+    for i in range(len(basisVector)): # making the term-frequency values of each word into TFIDF values
         queryVector[i] = math.log(1 + queryVector[i], 2) * idfData[basisVector[i] + "IDF"]
 
-    return {"queryVector": queryVector, "basisVector": basisVector}
 
-def search(phrase, boost):
-    queryData = get_Query_data(phrase)
-    queryVector =queryData["queryVector"]
-    basisVector =queryData["basisVector"]
-    csList = []
+def compareScore(freqData):
+    global csList
+    if len(csList) ==0:
+        csList+=[freqData]
+    elif len(csList) < 10:
+        csList = insert_List(freqData)
+    elif freqData['score'] > csList[-1]['score']:
+        csList.pop(-1)
+        csList = insert_List(freqData)
 
+
+def calculateScore(boost):
+    global csList
+    global pageRankData
     filepath = os.listdir("pageFreqFiles")
 
     for file in filepath: # grabbing json data for each file for comparison
@@ -43,55 +124,25 @@ def search(phrase, boost):
         freqData = json.load(fHand)
         fHand.close()
 
-        pageFreqData = {
-            "TFIDFValues" : [],
-            "url": freqData["URL"],
-            "title" : freqData["Title"],
-            "score" : 0
-        }
+        pageFreqData = {'url':freqData["URL"], 'title':freqData["Title"],  'score':[]}
 
         for i in basisVector:
             if (i + "TFIDF") in freqData:
-                pageFreqData["TFIDFValues"].append(freqData[i + "TFIDF"])
+                pageFreqData['score'].append(freqData[i + "TFIDF"])
             else:
-                pageFreqData["TFIDFValues"].append(0)
+                pageFreqData['score'].append(0)
 
-        if boost: # figuring boost stuff
-            pageFreqData["score"] = calc_CS(queryVector, pageFreqData["TFIDFValues"]) * searchdata.get_page_rank(pageFreqData["url"])
-        else:
-            pageFreqData["score"] = calc_CS(queryVector, pageFreqData["TFIDFValues"])
+        pageFreqData['score'] = calc_CS(queryVector, pageFreqData,boost)
+        compareScore(pageFreqData)
 
-        del pageFreqData["TFIDFValues"]
-        if len(csList) ==0:
-            csList.append(pageFreqData)
-        elif len(csList) < 10:
-            csList = insert_List(csList, pageFreqData)
-        elif pageFreqData["score"] > csList[-1]["score"]:
-            csList.pop(-1)
-            csList = insert_List(csList, pageFreqData)
 
+#Main Function
+def search(input, boost):
+    init(input, boost)
+    populateBasisVector()
+    populateQueryVector()
+    calculateScore(boost)
     return csList
-
-
-def insert_List(csList, csInfo):
-    for i in range(len(csList)):
-        if csInfo["score"] >= csList[i]["score"]:
-            return csList[0:i] + [csInfo] + csList[i:]
-
-    return csList + [csInfo]
-
-def calc_CS(queryVector, pageVector):
-    numerator =0
-    qEuclidNorm =0
-    pEuclidNorm =0
-    for i in range(len(queryVector)):
-        numerator += queryVector[i] * pageVector[i]
-        qEuclidNorm += queryVector[i] **2
-        pEuclidNorm += pageVector[i] **2
-    if qEuclidNorm ==0 or pEuclidNorm ==0:
-        return 0
-
-    return numerator / ((qEuclidNorm**0.5) * (pEuclidNorm**0.5))
 """
 list = search('coconut coconut peach tomato pear banana',True)
 for i in list:
